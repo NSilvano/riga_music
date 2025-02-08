941defd3f46c8a525370b0b07b7298a6ea0c17ec
Ntsika Silvano <155521048+NSilvano@users.noreply.github.com>
2025-02-08 13:19:33 +0200

    Revert "Feat/authentication"

--- packages/models/lib/models.dart
+++ packages/models/lib/models.dart
@@ -1,2 +1,6 @@
+library models;
+
 export 'src/api_responses/api_response_exports.dart';
 export 'src/dto/dto_export.dart';
+export 'src/general/general_export.dart';
+export 'src/mappers/user_details_mapper_exports.dart';
