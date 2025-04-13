76fc5baff2797e6e5eb9850cb90516e12f50d0c3
Ntsika <155521048+NSilvano@users.noreply.github.com>
2025-04-13 11:36:45 +0200

    chore: remove hardcoded API key

--- lib/presentation/home/homepage.dart
+++ lib/presentation/home/homepage.dart
@@ -2,11 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:riga_music_app/presentation/auth/authentication_screen.dart';
 import 'package:core/src/application/auth/bloc/auth_bloc.dart';
-import 'package:core/src/services/interfaces/i_authentication_service.dart';
 import 'package:core/src/services/implementations/authentication_service.dart';
-import 'package:core/src/services/interfaces/i_youtube_service.dart';
 import 'package:core/src/services/implementations/youtube_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -17,12 +16,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-    final IAuthenticationService authService =
+    final firebaseAuth = FirebaseAuth.instance;
+    final authService =
         AuthenticationService(firebaseAuth, FirebaseFirestore.instance);
-    final AuthBloc authBloc = AuthBloc(authService);
-    final IYouTubeService youTubeService = YouTubeService();
-    final YoutubeBloc youtubeBloc = YoutubeBloc(youTubeService);
+    final authBloc = AuthBloc(authService);
+    final youtubeApiKey = dotenv.env['YOUTUBE_API_KEY'] ?? '';
+    final youTubeService = YouTubeService(apiKey: youtubeApiKey);
+    final youtubeBloc = YoutubeBloc(youTubeService);
 
     return MultiBlocProvider(
       providers: [
--- packages/core/lib/src/injection/injection.dart
+++ packages/core/lib/src/injection/injection.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:core/src/services/implementations/youtube_service.dart';
 import 'package:core/src/services/interfaces/i_youtube_service.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'injection.config.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -8,6 +9,7 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
 final getIt = GetIt.instance;
+final youtubeApiKey = dotenv.env['YOUTUBE_API_KEY'] ?? '';
 
 @InjectableInit(
   initializerName: 'init',
@@ -25,5 +27,5 @@ abstract class RegisterModule {
   FirebaseFirestore get firestore => FirebaseFirestore.instance;
 
   @lazySingleton
-  IYouTubeService get youTubeService => YouTubeService();
+  IYouTubeService get youTubeService => YouTubeService(apiKey: youtubeApiKey);
 }
